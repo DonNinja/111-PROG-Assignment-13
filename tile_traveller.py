3045f6b903c1adb89f14b88e2612dd38c792572a
DonNinja <yngvileo@gmail.com>
2019-10-08 10:19:39 +0000

    Kláruðum 2

--- tile_traveller.py
+++ tile_traveller.py
@@ -8,6 +8,28 @@ SOUTH = 's'
 WEST = 'w'
 COINMESSAGE = "Pull a lever (y/n): "
 
+def main():
+    play = True
+    while play == True:
+        victory = False
+        row = 1
+        col = 1
+        total_coins = 0
+
+        valid_directions = NORTH
+        print_directions(valid_directions)
+
+        while not victory:
+            victory, col, row = play_one_move(col, row, valid_directions)
+            if victory:
+                print("Victory! Total coins", str(total_coins) + ".")
+                pa_input = input('Play again (y/n): ')
+                play = play_again(pa_input)
+            else:
+                total_coins = coinMessage(col, row, total_coins)
+                valid_directions = find_directions(col, row)
+                print_directions(valid_directions)
+
 def move(direction, col, row):
     ''' Returns updated col, row given the direction '''
     if direction == NORTH:
@@ -100,32 +122,11 @@ def get_coin(answer, total):
     else:
         return total 
 
-def play_again(pa_input, total_coins, col, row, victory):
+def play_again(pa_input):
     if pa_input.lower() == 'y':
-        total_coins = 0
-        col = 1
-        row = 1
-        victory = False
-        return (total_coins, col, row, victory)
+        return True
     else:
-        exit()
+        return False
     
 # The main program starts here
-victory = False
-row = 1
-col = 1
-total_coins = 0
-
-valid_directions = NORTH
-print_directions(valid_directions)
-
-while not victory:
-    victory, col, row = play_one_move(col, row, valid_directions)
-    if victory:
-        print("Victory! Total coins", str(total_coins) + ".")
-        pa_input = input('Play again (y/n): ')
-        play_again(pa_input, total_coins, col, row, victory)
-    else:
-        total_coins = coinMessage(col, row, total_coins)
-        valid_directions = find_directions(col, row)
-        print_directions(valid_directions)
+main()
